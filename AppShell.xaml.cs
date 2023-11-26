d0807a37ebe00d532770df4c633da997f9dcb093
Bruno Garcia <bruno@brunogarcia.com>
2023-11-26 14:25:56 -0500

    laying the keel

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AnchorAlertApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AnchorAlertApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
